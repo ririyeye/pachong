3a672df7a3493b4cfd8f44fd508a8f3a2d9881c6
ririyeye <200610237@qq.com>
2016-08-17 18:01:50 +0800

    数据部分完成

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxcomplete = new System.Windows.Forms.TextBox();
+            this.textBoxIndex = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxcomplete
+            // 
+            this.textBoxcomplete.Location = new System.Drawing.Point(27, 49);
+            this.textBoxcomplete.Name = "textBoxcomplete";
+            this.textBoxcomplete.Size = new System.Drawing.Size(100, 21);
+            this.textBoxcomplete.TabIndex = 0;
+            // 
+            // textBoxIndex
+            // 
+            this.textBoxIndex.Location = new System.Drawing.Point(162, 49);
+            this.textBoxIndex.Name = "textBoxIndex";
+            this.textBoxIndex.Size = new System.Drawing.Size(100, 21);
+            this.textBoxIndex.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "完成次数";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "正在进行页面";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 94);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxIndex);
+            this.Controls.Add(this.textBoxcomplete);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxcomplete;
+        private System.Windows.Forms.TextBox textBoxIndex;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Windows.Threading;
+using System.Threading.Tasks.Dataflow;
+//using NHtmlUnit;
+//using NHtmlUnit.Html;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        Thread thCatch;
+        public Form1()
+        {
+            this.FormClosing += (a,b) => Environment.Exit(0);
+
+            InitializeComponent();
+            thCatch = new Thread(catchMain);
+            thCatch.Start();
+        }
+        ActionBlock<int> proBlock;
+
+        void catchMain()
+        {
+            int recod = 0;
+            int comIndex = 0;
+
+            while (true)
+            {
+                proBlock = new ActionBlock<int>((i) =>
+                {
+                    comIndex++;
+                    textBoxIndex.Invoke(new Action(() =>
+                    {
+                        textBoxIndex.Text = comIndex.ToString();
+                    }));
+
+
+                    for(int trytime = 0;trytime <10;trytime ++)
+                    {
+                        try
+                        {
+                            catchIndex(i);
+                            break;
+                        }
+                        catch (Exception ex)
+                        {
+                            continue;
+                        }
+                    }
+                }
+                , new ExecutionDataflowBlockOptions() { MaxDegreeOfParallelism = 4 });
+                textBoxIndex.Invoke(new Action(() =>
+                {
+                    textBoxcomplete.Text = recod++.ToString();
+                }));
+                int num = firstcatch();
+                for (int i = 1; i < num; i++)
+                {
+                    proBlock.Post(i);
+                }
+                proBlock.Complete();
+                proBlock.Completion.Wait();
+                //for (int i=1;i<num;i++)
+                //{
+                //    textBoxIndex.Invoke(new Action(() =>
+                //    {
+                //        textBoxIndex.Text = i.ToString();
+                //    }));
+                //    catchIndex(i);
+                //}
+            }
+        }
+        //2016-08-10~2016-08-17.html
+        string getdataString()
+        {
+            DateTime currentTime = DateTime.Now;
+            DateTime beforeTime = currentTime.AddDays(-7);
+            return string.Format("{0:d}-{1:d}-{2:d}~{3:d}-{4:d}-{5:d}", currentTime.Year, currentTime.Month, currentTime.Day,
+                beforeTime.Year, beforeTime.Month, beforeTime.Day
+                );
+        }
+        string getpageString(int num)
+        {
+            //http://www.bilibili.com/list/default-33-1-2016-8-10~2016-8-17.html
+            return "http://www.bilibili.com/list/default-33-" + num.ToString() + "-2016-8-10~2016-8-17.html";
+        }
+
+
+        int firstcatch()
+        {
+            string st = GetHtmlCode(getpageString(1));
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(st);
+            HtmlNode node = doc.DocumentNode;
+            var col = node.SelectNodes("ul/li");
+            if (col != null)
+            {
+                foreach (var eachanimate in col)
+                {
+                    watchrecord animateNode = new watchrecord(eachanimate);
+                }
+            }
+
+            try
+            {
+                var nump = node.SelectNodes("div[1] / a[10]");
+                return int.Parse(nump[0].InnerHtml);
+            }
+            catch (System.Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        List<watchrecord> catchIndex(int index)
+        {
+            string st = GetHtmlCode(getpageString(index));
+            List<watchrecord> ret = new List<watchrecord>(20);
+            if (st != "")
+            {
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(st);
+                HtmlNode node = doc.DocumentNode;
+                var col = node.SelectNodes("ul/li");
+                if (col != null)
+                {
+                    foreach (var eachanimate in col)
+                    {
+                        ret.Add(new watchrecord(eachanimate));
+                    }
+                }
+            }
+            return ret;
+        }
+
+
+        class watchrecord
+        {
+            string title;
+            string watchCount;
+            string collectCount;
+            string danmuCount;
+            HtmlNode fatherNode;
+            public watchrecord(HtmlNode fathernode)
+            {
+                this.fatherNode = fathernode;
+
+                title = getMessage("div / div[1] / a[2]");
+                watchCount = getMessage("div / div[2] / div[2] / span[1] / span");
+                danmuCount = getMessage("div/div[2]/div[2]/span[2]/span");
+                collectCount = getMessage("div/div[2]/div[2]/span[3]/span");
+            }
+
+            string getMessage(string sel)
+            {
+                var title = fatherNode.SelectNodes(sel);
+                return title[0].InnerHtml;
+            }
+        }
+
+        private string GetHtmlCode(string url)
+        {
+            string htmlCode;
+            HttpWebRequest webRequest = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url);
+            webRequest.Timeout = 5000;
+            webRequest.Method = "GET";
+            webRequest.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36";
+            webRequest.Headers.Add("Accept-Encoding", "gzip, deflate");
+            HttpWebResponse webResponse = (System.Net.HttpWebResponse)webRequest.GetResponse();
+            if (webResponse.ContentEncoding.ToLower() == "gzip")//如果使用了GZip则先解压            {
+                using (System.IO.Stream streamReceive = webResponse.GetResponseStream())
+                {
+                    using (var zipStream =
+                        new System.IO.Compression.GZipStream(streamReceive, System.IO.Compression.CompressionMode.Decompress))
+                    {
+                        using (StreamReader sr = new System.IO.StreamReader(zipStream, Encoding.GetEncoding("utf-8")))
+                        {
+                            htmlCode = sr.ReadToEnd();
+                        }
+                    }
+                }
+
+            else
+            {
+                using (System.IO.Stream streamReceive = webResponse.GetResponseStream())
+                {
+                    using (System.IO.StreamReader sr = new System.IO.StreamReader(streamReceive, Encoding.Default))
+                    {
+                        htmlCode = sr.ReadToEnd();
+                    }
+                }
+            }
+            return htmlCode;
+        }
+        /*
+ *    备用方案              *
+ *                          *                 
+//client.BrowserVersion
+NHtmlUnit.WebClient client = new NHtmlUnit.WebClient();
+client.Options.JavaScriptEnabled = false;
+client.Options.CssEnabled = false;
+client.Options.ThrowExceptionOnScriptError = false;
+client.Options.Timeout = 5000;
+HtmlPage page = client.GetHtmlPage("http://www.bilibili.com/video/bangumi-two-1.html");
+string xml = page.AsXml();
+*/
+    }
+}
+
